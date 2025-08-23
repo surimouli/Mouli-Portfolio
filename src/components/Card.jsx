b4f80e5554d897dfa40fa541a326e9a2f0f4c91a
Mouli Suri <surimi@mail.uc.edu>
2025-08-23 19:28:50 -0400

    Grid 3 added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,16 @@
-const Card = ({ text }) => {
+import { motion } from "motion/react";
+
+const Card = ({ style, text, containerRef }) => {
     return (
-        <div className = "absolute px-1 py-4 text-xl text-center rounded-full ring ring-grey-700 font-extralight bg-storm w-[12rem] cursor-grab">
+        <motion.div className = "absolute px-1 py-4 text-xl text-center rounded-full ring ring-grey-700 font-extralight bg-storm w-[12rem] cursor-grab"
+            style={style}
+            whileHover={{ scale: 1.05 }}
+            drag
+            dragConstraints={containerRef}
+            dragElastic={0.6}
+        >
             {text}
-        </div>
+        </motion.div>
     );
 };
 
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,9 @@
+import { useRef } from "react";
 import Card from "../components/Card";
+import { Globe } from "../components/Globe";
 
 const About = () => {
+    const grid2Container = useRef();
     return (
         <section className = "c-space section-spacing">
             <h2 className = "text-heading" >About Me</h2>
@@ -16,22 +19,71 @@ const About = () => {
                             Hi, I'm Mouli Suri
                         </p>
                         <p className = "subtext">
-                            Welcome here
+                            A passionate programmer and aspiring software developer with a knack for problem-solving and a love for creating efficient, scalable solutions. Experienced in C++, Python, JavaScript, and web development technologies like React and Next.js. Eager to contribute my skills and enthusiasm to innovative projects in a collaborative environment.
                         </p>
                     </div>
                     <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
                 </div>
                 {/* Grid 2 */}
                 <div className = "grid-default-color grid-2">
-                    <div className = "flex items-center justify-center w-full h-full">
+                    <div ref = {grid2Container} className = "flex items-center justify-center w-full h-full">
                         <p className = "flex items-end text-5xl text-grey-500">
                             CODE IS ART
                         </p>
-                        <Card />
+                        <Card 
+                            style = {{ rotate: "75deg", top: "25%", left: "23%" }}
+                            text = "C++"
+                            containerRef = {grid2Container}
+                        />
+                        <Card 
+                            style = {{ rotate: "-25deg", top: "10%", left: "50%" }}
+                            text = "Python"
+                            containerRef = {grid2Container}
+                        />
+                        <Card 
+                            style = {{ rotate: "55deg", top: "58%", left: "40%" }}
+                            text = "HTML"
+                            containerRef = {grid2Container}
+                        />
+                        <Card 
+                            style = {{ rotate: "75deg", top: "30%", left: "72%" }}
+                            text = "JavaScript"
+                            containerRef = {grid2Container}
+                        />
+                        <Card 
+                            style = {{ rotate: "-30deg", top: "65%", left: "5%" }}
+                            text = "CSS"
+                            containerRef = {grid2Container}
+                        />
+                        <Card 
+                            style = {{ rotate: "-45deg", top: "60%", left: "60%" }}
+                            text = "Next.js"
+                            containerRef = {grid2Container}
+                        />
+                        <Card 
+                            style = {{ rotate: "45deg", top: "18%", left: "1%" }}
+                            text = "React"
+                            containerRef = {grid2Container}
+                        />
+                        <Card 
+                            style = {{ rotate: "20deg", top: "70%", left: "28%" }}
+                            text = "TailwindCSS"
+                            containerRef = {grid2Container}
+                        />
                     </div>
                 </div>
                 {/* Grid 3 */}
-                <div className = "grid-black-color grid-3"></div>
+                <div className = "grid-black-color grid-3">
+                    <div className = "z-10 w-[50%]">
+                        <p className = "headtext">Mouli</p>
+                        <p className = "subtext" >
+                            yayy! you found me :)
+                        </p>
+                    </div>
+                    <figure className = "absolute left-[30%] top-[10%]">
+                        <Globe />
+                    </figure>
+                </div>
                 {/* Grid 4 */}
                 <div className = "grid-special-color grid-4"></div>
                 {/* Grid 5 */}
